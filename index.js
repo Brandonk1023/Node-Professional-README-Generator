815ce363027a28cccd22ffd8044bc84ddbe7f228
Brandon Kilgore <brandonk1023@gmail.com>
2023-03-27 10:16:59 -0400

    added beginning questions for README generation

--- index.js
+++ index.js
@@ -3,7 +3,34 @@ const fs = require('fs')
 const inquirer = require('inquirer')
 const generateMarkdown = require('utils\generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the name of the project?',
+
+    },
+
+    {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'Please provide a description of the project.'
+    },
+
+    {
+        type: 'input',
+        name: 'installationRequirements',
+        message: 'What are the steps needed to install the project?',
+    },
+
+    {
+        type: 'input',
+        name: 'projectUsage',
+        message: 'Please explain how to use the applicaton/project.'
+    },
+
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
